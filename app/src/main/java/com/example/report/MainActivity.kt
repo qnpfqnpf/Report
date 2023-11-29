8a4cc27ccf75e8f0b8114000fcca2ed3ac2dbbe5
qnpfqnpf <qnpfqnpf1@gmail.com>
2023-11-30 04:31:47 +0900

    MainActivity에 적용

--- app/src/main/java/com/example/report/MainActivity.kt
+++ app/src/main/java/com/example/report/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.report
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.report.databinding.ActivityMainBinding
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
@@ -10,6 +13,7 @@ import com.google.firebase.auth.auth
 class MainActivity : AppCompatActivity() {
 
     private val mBinding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val adapter = ItemRecyclerViewAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,9 +21,28 @@ class MainActivity : AppCompatActivity() {
         if (Firebase.auth.currentUser == null) {
             startActivity(
                 Intent(this, LoginActivity::class.java))
-            finish()
         }
 
-        // logout Firebase.auth.signOut()
+        submitList()
+
+        mBinding.apply {
+            btnLogout.setOnClickListener {
+                Firebase.auth.signOut()
+                startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+            }
+            viewItem.adapter = adapter
+            viewItem.layoutManager = LinearLayoutManager(baseContext, RecyclerView.VERTICAL, false)
+            val dividerItemDecoration = DividerItemDecoration(viewItem.context, LinearLayoutManager.VERTICAL)
+            viewItem.addItemDecoration(dividerItemDecoration)
+        }
+    }
+
+    private fun submitList() {
+        //TODO: Get From FireStore
+        val list = ArrayList<Item>()
+        repeat(10) {
+            list += Item("${it}", "${it}", "${it}", "${it}", it % 2 == 0)
+        }
+        adapter.submitList(list)
     }
 }
