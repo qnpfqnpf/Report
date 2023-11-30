4078bd58798fce010bd041b8a3844198adb34e41
qnpfqnpf <qnpfqnpf1@gmail.com>
2023-11-30 19:42:58 +0900

    수정 가능하도록 변경

--- app/src/main/java/com/example/report/CustomEditDialog.kt
+++ app/src/main/java/com/example/report/CustomEditDialog.kt
@@ -3,32 +3,25 @@ package com.example.report
 import android.app.Dialog
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.Window
-import com.example.report.databinding.DialogCustomBinding
-import com.google.firebase.Firebase
-import com.google.firebase.auth.auth
+import com.example.report.databinding.DialogCustomEditBinding
 
-class CustomDialog(private val context: Context, item: Item) {
+class CustomEditDialog(private val context: Context, item: Item) {
     private val dialog = Dialog(context)
-    private val mBinding: DialogCustomBinding by lazy { DialogCustomBinding.inflate(LayoutInflater.from(context)) }
+    private val mBinding: DialogCustomEditBinding by lazy { DialogCustomEditBinding.inflate(LayoutInflater.from(context)) }
 
     private val displayMetrics = context.resources.displayMetrics
     private val width = (displayMetrics.widthPixels * 0.8).toInt()
     private val height = (displayMetrics.heightPixels * 0.5).toInt()
 
     init {
+
         mBinding.apply {
             this.item = item
-            btnEdit.visibility = View.INVISIBLE
-
-            if(item.user == Firebase.auth.currentUser.toString()) {
-                btnEdit.visibility = View.VISIBLE
-            }
-
             btnEdit.setOnClickListener {
+                //TODO: edit
+
                 dismiss()
-                CustomEditDialog(context, item).show()
             }
         }
         initDialog()
